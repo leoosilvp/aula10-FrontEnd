aa379991e2bfed3f06b7d0fc45608395df5f0472
leoosilvp <leo965679@gmail.com>
2025-06-13 18:08:25 -0300

    1 u- finalizando tudo

--- src/js/main.js
+++ src/js/main.js
@@ -19,9 +19,11 @@ function adicionarTarefa() {
         //alert("Preencha o Campo Tarefa!")
         Swal.fire({
             icon: "warning",
-            tittle: "Atenção",
-            text: "Preencha o campo tarefa!",
-            confirmButtonCollor: "#1e3e2e",
+            iconColor: "#ffe600",
+            color: "black",
+            title: "Atenção",
+            text: "Preencha o campo Tarefa!",
+            confirmButtonColor: "black",
             confirmButton: "Ok",
         })
     } else {
@@ -30,10 +32,30 @@ function adicionarTarefa() {
         listarTarefas();
         Swal.fire({
             icon: "success",
+            iconColor: "#00c200",
             showConfirmButton: false,
             timer: 1000
         })
     }
+
+    for (let i = 0; i < tarefas.length; i++) {
+        //limita
+        if (i >= 8) {
+            Swal.fire({
+                icon: "warning",
+                iconColor: "#ffe600",
+                color: "black",
+                title: "Atenção",
+                text: "Lista cheia!",
+                confirmButtonColor: "black",
+                confirmButton: "Ok",
+            })
+        }
+    }
+
+
+
+
 }
 
 // listar Tarfeas
@@ -41,7 +63,7 @@ function adicionarTarefa() {
 function listarTarefas() {
     let valor = "";
     for (let i = 0; i < tarefas.length; i++) {
-        valor += tarefas[i] + "<br>";
+        valor += "<p>" + i + "- " + tarefas[i] + "</p>";
     }
     document.getElementById("lista").innerHTML = valor;
 }
@@ -51,22 +73,27 @@ function listarTarefas() {
 function removerTarefa() {
     Swal.fire({
         icon: "warning",
-        title: "tem certeza que deseja apagar?",
-        text: "sua tarefa será apagada!",
-        showCancelButton: "#1e3e2e",
+        iconColor: "#ffe600",
+        color: "black",
+        title: "Atenção",
+        text: "Tem certeza que deseja apagar?",
         showCancelButton: true,
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Apagar",
-        cancelButtonText: "Cancelar"
+        showConfirmButton: true,
+        confirmButtonColor: "#000",
+        cancelButtonColor: "#000",
+        confirmButtonText: "SIM!",
+        cancelButtonText: "NÃO!"
     }).then((result) => {
         if (result.isConfirmed) {
             tarefas.pop();
             listarTarefas();
-            Swal.fire(
-                "Apagado",
-                "A tarefe foi apagada da lista",
-                "success",
-            )
+            Swal.fire({
+                icon: "success",
+                iconColor: "#00c200",
+                showConfirmButton: false,
+                timer: 1000
+            })
         }
     })
 }
+
